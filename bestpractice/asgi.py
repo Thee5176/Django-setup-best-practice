7fcf7a93bc9e3200e7cf407bd09a00a7e5cbfab1
Thee5176 <theerapong5176@gmail.com>
2024-10-06 16:02:50 +0900

    Rename project from beginner -> bestpractice

--- bestpractice/asgi.py
+++ bestpractice/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for beginner project.
+ASGI config for bestpractice project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beginner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bestpractice.settings')
 
 application = get_asgi_application()
